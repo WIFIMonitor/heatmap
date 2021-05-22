bb0849cfa79f7e047dea88cbc2af39ac3c479698
tiago <tiagobarros99@ua.pt>
2021-05-22 18:58:07 +0100

    timelapse generation

--- TimelapseHeatmap/heatmap.py
+++ TimelapseHeatmap/heatmap.py
@@ -1,52 +1,97 @@
-import matplotlib.pyplot as plt
-from PIL import Image
-from sklearn.datasets._samples_generator import make_blobs
-import seaborn as sns
+from influxdb import InfluxDBClient
+import plotly.express as px
+import ffmpeg
+import datetime
+import os
+import time
 
 
-def create_clusters(centers,n_samples,deviations):
-    X,truth = make_blobs(n_samples=n_samples, centers=centers,cluster_std=deviations,random_state=42)
-
-    dx = []   # x axis points
-    dy = []     # y axis points
-    for coords in X:
-      dx.append(coords[0])
-      dy.append(coords[1])
+client = InfluxDBClient("***REMOVED***", ***REMOVED***, "***REMOVED***", "***REMOVED***", "***REMOVED***")
+# get the last timestamp value of the database
+lasttimestamp= client.query("select last(clientsCount), time from clientsCount").raw['series'][0]['values'][0][0]
+prev_ts = lasttimestamp
+# load ap cooordinates into memory, so that we dont 
+# have to read the excel file every time someone
+# wants to check the heatmap
+def load_ap_coords():
+    coords = {}
+    f = open("fileCoords.txt","r")
     
+    for line in f:
+        info = line.split(",")
+        dic = { 
+        "lat" : info[1],
+        "lon" : info[2].strip('\n'),
+        "piso" : info[3] if info[3]!="None\n" else "Não Definido",
+        "people" : 0
+        }
+        coords[info[0]] = dic
     
+    f.close()
+    return coords
+
+def get_dictionary(coordinates,timesub):
+    # since we get values every 15 minutes, we need to now how many 15 minute measures we want
+    sub1 = str(15*timesub)
+    sub2 = str(15*(timesub-1))
+    # query values between last measurement, minus 15 minutes
+    sq = "select id,clientsCount from clientsCount where time <=\'" + str(prev_ts) + "\'-"+sub2+"m and time > \'" + str(prev_ts) + "\'-"+sub1+"m" 
+    # get the last 15m values, from the last value in DB, and not from now(), because CISCO PRIME can stop sending values
+    try:
+        people_count = client.query(sq).raw['series'][0]["values"]
+    except:
+        return
+
+    dataset = []
+    for line in people_count:
+        if line[1] in coordinates:
+            coordinates[line[1]]["people"]=line[2]
+            dataset.append(coordinates[line[1]])
+        else:
+            continue
     
-    ### 
-    ### WHEN TIME COMES WHERE IT'S NEEDED TO LIMIT THE CLUSTERS WITHIN A CERTAIN
-    ### ROOM, JUST SIMPLY ITERATE THROUGH DX AND DY ARRAY, AND REMOVE THE POINTS
-    ### WICH ARE OUTISDE THE ROOM
-    ###
-    return dx,dy
+    return dataset
 
+def createVideo():
+    if os.path.exists("timelapse.mkv"):
+        os.remove("timelapse.mkv")
+    
+    (
+    ffmpeg
+    .input('output/*.png', pattern_type='glob', framerate=5)
+    .output('timelapse.mkv')
+    .run()
+    )
 
-centers = [(300,650), 
-    (250,500), 
-    (400,380),
-    (300,300),
-    (470,290),
-    (550,150)]   # AP's coordinates
+    print("Video Created! ")
 
-n_samples = [3,6,20,10,4,3]  # number of people conected per API, sorted according to centers
+coords = load_ap_coords()
+start = time.time()
+# Generate 10 timelaspes. In the future it will be decided by the user input
+for x in range(1,70):
+    try:
+        df = get_dictionary(coords,x)
+        fig = px.density_mapbox(df, lat='lat', lon='lon', z='people', radius=10,
+                            center=dict(lat=40.63041451444991, lon=-8.65803098047244),
+                            zoom=15,
+                            mapbox_style="stamen-terrain",
+                            width=800,
+                            height=1100,
+                            color_continuous_scale= [
+                [0.0, "green"],
+                [0.3, "green"],
+                [0.5, "yellow"],
+                [0.7, "yellow"],
+                [0.9, "red"],
+                [1.0, "red"]],
+                            title= str(prev_ts) + "- " + str((x*15)) + "m"
+                            ) 
+        fs = "output/file" + str(x) + ".png"
+        fig.write_image(fs,scale=0.5)
+    except Exception as e:
+        print(e)
+        continue
 
-deviations = [3*2,6*2,20*2,10*2,4*2,3*2] # deviation of the clusters, the more people are connected to a certain AP, bigger the deviation.
-
-
-dx,dy = create_clusters(centers,n_samples,deviations) # create clusters
-
-
-plt.scatter(dx, dy)
-plt.title(f"Example of a mixture of {len(centers)} distributions")
-plt.xlabel("x")
-plt.ylabel("y")
-
-
-Image1 = Image.open("input/piso1.png")
-width, height = Image1.size
-plt.imshow(Image1, interpolation=None) # I would add interpolation='none'
-sns.kdeplot(x=dx,y=dy,shade=True,thresh=0.05,alpha=0.5,cmap="magma",levels=5,clip=((0,height)),bw_adjust=0.8)
-plt.savefig('output/piso1.png',bbox_inches='tight')
-plt.show()
+createVideo()
+end = time.time()
+print("Time elapsed: ",end-start)
